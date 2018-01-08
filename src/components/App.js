eceadc2410bfe2eeddbf681f870b8afb42824bc6
Dave Fravel <dave@bluegear.io>
2018-01-08 17:28:48 -0500

    finished episode 21

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,11 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
     this.state = {
       fishes: {},
       order: {}
@@ -62,6 +65,18 @@ class App extends React.Component {
     this.setState({ fishes: fishes });
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
+  removeFish(key) {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = null;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -74,6 +89,12 @@ class App extends React.Component {
     // update/add the new number of fish ordered
     order[key] = order[key] + 1 || 1;
     // update the store
+    this.setState({ order });
+  }
+
+  removeFromOrder(key) {
+    const order = { ...this.state.order };
+    delete order[key];
     this.setState({ order });
   }
 
@@ -97,8 +118,15 @@ class App extends React.Component {
           fishes={this.state.fishes}
           order={this.state.order}
           params={this.props.params}
+          removeFromOrder={this.removeFromOrder}
         />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+          removeFish={this.removeFish}
+        />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -3,10 +3,79 @@ import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
   state = {};
+  constructor() {
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, key) {
+    const fish = this.props.fishes[key];
+
+    // take a copy of the fish and update it with the new data
+    const updatedFish = {
+      ...fish,
+      [e.target.name]: e.target.value
+    };
+
+    this.props.updateFish(key, updatedFish);
+  }
+
+  renderInventory(key) {
+    const fish = this.props.fishes[key];
+
+    return (
+      <div className="fish-edit" key={key}>
+        <input
+          type="text"
+          name="name"
+          id=""
+          placeholder="Fish Name"
+          value={fish.name}
+          onChange={e => this.handleChange(e, key)}
+        />
+        <input
+          type="text"
+          name="price"
+          id=""
+          placeholder="Fish Price"
+          value={fish.price}
+          onChange={e => this.handleChange(e, key)}
+        />
+        <select
+          name="status"
+          onChange={e => this.handleChange(e, key)}
+          value={fish.status}
+        >
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out</option>
+        </select>
+        <textarea
+          placeholder="Fish Description"
+          name="desc"
+          defaultValue={fish.desc}
+          onChange={e => this.handleChange(e, key)}
+        />
+        <input
+          type="text"
+          name="image"
+          id=""
+          placeholder="Fish Image URL"
+          value={fish.image}
+          onChange={e => this.handleChange(e, key)}
+        />
+
+        <button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
         <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
+
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
